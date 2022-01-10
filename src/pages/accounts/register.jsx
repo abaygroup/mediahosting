093079cb44cff81b2a534bbd419d1749b21333da
Mukhagali Aryn <mukhagaliaryn@gmail.com>
2022-01-10 18:27:19 +0600

    Done

--- src/pages/accounts/register.jsx
+++ src/pages/accounts/register.jsx
@@ -58,8 +58,7 @@ const Register = () => {
             .required('Имя требуется')
             .max(32, "Максимум 32 символов"),
         phone: yup.string()
-            .required('Телефон требуется')
-            .max(32, "Максимум 12 символов"),
+            .required('Телефон требуется'),
         password: yup.string()
             .required('Необходим пароль')
             .min(8, 'Пароль должен состоять из 8 или более символов')
@@ -79,6 +78,13 @@ const Register = () => {
     const dispatch = useDispatch()
     const register_success = useSelector(state => state.auth.register_success);
     const { register, formState:{ errors }, handleSubmit } = useForm({ resolver: yupResolver(schema) });
+
+    const [phone, setPhone] = useState('');
+
+    const phoneChange = (e) => {
+        const x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
+        setPhone(!x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : ''))
+    }
 
     const onSubmit = (data) => {
         if (dispatch && dispatch !== null && dispatch !== undefined)
@@ -102,55 +108,59 @@ const Register = () => {
     return (
         <React.Fragment>
             <AccountLayout title="Регистрация - mediahosting" content="Регистрация">
-            <div className="accounts-container">
-                <div className="head">
-                    <Link href="/"><a className="back"><Image width={100} height={100} src="https://img.icons8.com/ios/100/000000/back--v1.png"/></a></Link>
-                    <Link href="/"><a><Image width={5276} height={730} src="/icons/logo-black.png" /></a></Link>
+                <div className="accounts-container">
+                    <div className="head">
+                        <Link href="/"><a className="back"><Image width={100} height={100} src="https://img.icons8.com/ios/100/000000/back--v1.png"/></a></Link>
+                        <Link href="/"><a><Image width={5276} height={730} src="/icons/logo-black.png" /></a></Link>
+                    </div>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <h4>{t("common:accounts.register.h4")}</h4>
+                        <div className="form-group">
+                            <label htmlFor="">{t("common:accounts.register.form.username.label")}</label>
+                            <input type="text" className={errors["username"] && "warning"} {...register("username")} placeholder={t("common:accounts.register.form.username.placeholder")} />
+                            {errors["username"] ? <p>{errors["username"].message}</p>: null}
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="">{t("common:accounts.register.form.email.label")}</label>
+                            <input type="email" className={errors["email"] && "warning"} {...register("email")} placeholder={t("common:accounts.register.form.email.placeholder")} />
+                            {errors["email"] ? <p>{errors["email"].message}</p>: null}
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="">{t("common:accounts.register.form.profile_name.label")}</label>
+                            <input type="text" className={errors["profile_name"] && "warning"} {...register("profile_name")} placeholder={t("common:accounts.register.form.profile_name.placeholder")} />
+                            {errors["profile_name"] ? <p>{errors["profile_name"].message}</p>: null}
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="">{t("common:accounts.register.form.phone.label")}</label>
+                            <input type="tel" id="phone" className={errors["phone"] && "warning"} {...register("phone")} value={phone} onChange={e => phoneChange(e)}  placeholder="+7 (XXX) XXX-XXXX" />
+                            {errors["phone"] ? <p>{errors["phone"].message}</p>: null}
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="">{t("common:accounts.register.form.password.label")}</label>
+                            <input type="password" className={errors["password"] && "warning"} {...register("password")} placeholder={t("common:accounts.register.form.password.placeholder")} minLength="8"/>
+                            {errors["password"] ? <p>{errors["password"].message}</p>: null}
+                            <small>{t("common:accounts.register.form.password.labels.first")}</small>
+                            <small>{t("common:accounts.register.form.password.labels.second")}</small>
+                            <small>{t("common:accounts.register.form.password.labels.third")}</small>
+                            <small>{t("common:accounts.register.form.password.labels.fourth")}</small>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="">{t("common:accounts.register.form.re_password.label")}</label>
+                            <input type="password" className={errors["re_password"] && "warning"} {...register("re_password")} placeholder={t("common:accounts.register.form.re_password.placeholder")} minLength="8"/>
+                            {errors["re_password"] ? <p>{errors["re_password"].message}</p>: null}
+                        </div>
+                        <div className="submit">
+                            <span></span>
+                            {loading ? <Loader />: <input type="submit" value={t("common:accounts.register.form.submit")} />}
+                        </div>
+                    </form>
+                    <div className="register-block">
+                        <h4>{t("common:accounts.is_accounts")}</h4>
+                        <Link href="/accounts/login">
+                            <a>{t("common:accounts.enter")}</a>
+                        </Link>
+                    </div>
                 </div>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <h4>{t("common:accounts.register.h4")}</h4>
-                    <div className="form-group">
-                        <label htmlFor="">{t("common:accounts.register.form.username.label")}</label>
-                        <input type="text" className={errors["username"] && "warning"} {...register("username")} placeholder={t("common:accounts.register.form.username.placeholder")} />
-                        {errors["username"] ? <p>{errors["username"].message}</p>: null}
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="">{t("common:accounts.register.form.email.label")}</label>
-                        <input type="email" className={errors["email"] && "warning"} {...register("email")} placeholder={t("common:accounts.register.form.email.placeholder")} />
-                        {errors["email"] ? <p>{errors["email"].message}</p>: null}
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="">{t("common:accounts.register.form.profile_name.label")}</label>
-                        <input type="text" className={errors["profile_name"] && "warning"} {...register("profile_name")} placeholder={t("common:accounts.register.form.profile_name.placeholder")} />
-                        {errors["profile_name"] ? <p>{errors["profile_name"].message}</p>: null}
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="">{t("common:accounts.register.form.phone.label")}</label>
-                        <input type="text" className={errors["phone"] && "warning"} {...register("phone")} placeholder="+X (XXX) XXX XX XX" />
-                        {errors["phone"] ? <p>{errors["phone"].message}</p>: null}
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="">{t("common:accounts.register.form.password.label")}</label>
-                        <input type="password" className={errors["password"] && "warning"} {...register("password")} placeholder={t("common:accounts.register.form.password.placeholder")} minLength="8"/>
-                        {errors["password"] ? <p>{errors["password"].message}</p>: null}
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="">{t("common:accounts.register.form.re_password.label")}</label>
-                        <input type="password" className={errors["re_password"] && "warning"} {...register("re_password")} placeholder={t("common:accounts.register.form.re_password.placeholder")} minLength="8"/>
-                        {errors["re_password"] ? <p>{errors["re_password"].message}</p>: null}
-                    </div>
-                    <div className="submit">
-                        <span></span>
-                        {loading ? <Loader />: <input type="submit" value={t("common:accounts.register.form.submit")} />}
-                    </div>
-                </form>
-                <div className="register-block">
-                    <h4>{t("common:accounts.is_accounts")}</h4>
-                    <Link href="/accounts/login">
-                        <a>{t("common:accounts.enter")}</a>
-                    </Link>
-                </div>
-            </div>
             </AccountLayout>
         </React.Fragment>
     )
