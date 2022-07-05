dddad6fbb0c7069414bae08efd443923558a2e6c
Mukhagali Aryn <mukhagaliaryn@gmail.com>
2022-07-05 18:52:03 +0600

    Done

--- src/pages/setting.jsx
+++ src/pages/setting.jsx
@@ -27,12 +27,14 @@ const Setting = () => {
         >
             <div className="setting-container">
                 <Link href={"/"}>
-                    <a className="close-btn"><Image width={100} height={100} src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGQAAABkCAYAAABw4pVUAAAABmJLR0QA/wD/AP+gvaeTAAACZklEQVR4nO3cuW7UQByAcXM0tPAiCakIPWcHQhScJWdAVNy0PAcS8BJ5BcSLABI0RBD4KNYrWZZndtn1zDj29ysjZ/2PPm328IyrSpIkSZIkSZIkSZIkSZIkaZSAI8A74FrpWRYBNoBd4ETpWZKoY3xgZn/IUYBN4Es96+dRRmnEmPsNXC09VxuwBXxrzfoJOFZ6tl4Bl4FfrT90H7heera51jOj6U3p2ZIALgWi3BjAbJvA18nEmItEuVlwppOBGK9LzZQVcBHY64hyq8As044xF4jyJ2cUY7REotzOcO5QjFepzz1owIWOKH+BOwnPuWWMiEiUuwnO1fU5wxhtOaJEYrzs6xyjApwPRLnXw2MbYxUpokRivOhz9tGqo/zsiHJ/hcc6BXw3xpoiUR78x2MYo0/AuUCUh0v87nYgxvMcs4/WKlGMkVgkyk7HsdvAD2MkFonyqHFMKMazkrOPFnA2EOUxcNoYBRB+99X+GcCT0vNOQuCZ0va09JyTApyJRDmwMQ6XHmANh0oPoNqS/7J8Mc/BF/UBIfJZBD+D5BWJsdM4xq9MclgmRuNYo6QUieGXi7mxxjURvBbSr0iMpa8aGqUnhK+rr3IJ18UN64jESLHIwSgxhNdmuQwot0iMvhfKuYR0EboXXSdZ34uLrOMCMZKugDdKQCRG8j0ikSjuDWnIuovKKDUGtM+QqW72nIvEKLYTd7JRCO9VH8q26EntUT/INw54W3q23uGtNYaF2c1n3jeeGUO++cwGY7/5TFVVFXAU+AhcKT3LIszeEu8Cx0vPIkmSJEmSJEmSJEmSJEmSlMY/3OIRevlpm58AAAAASUVORK5CYII=" /></a>
+                    <a className="close-btn">
+                        <Image width={100} height={100} src="https://img.icons8.com/ios/100/000000/delete-sign--v1.png"/>
+                    </a>
                 </Link>
                 <div className="logo">
                     <Link href={"/"}>
                         <a>
-                            <Image src="/icons/logo.png" width={5276} height={730} />
+                            <Image src="/icons/logo-black.png" width={5276} height={730} />
                         </a>
                     </Link>
                 </div>
