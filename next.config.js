caf816f1cbff8e4e2945f8377d9dbfe32161e010
Mukhagali Aryn <mukhagaliaryn@gmail.com>
2022-08-07 23:44:38 +0600

    D

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ module.exports = {
   reactStrictMode: true,
   ...nextTranslate(),
   images: {
-    domains: ['img.icons8.com', '127.0.0.1', 'abaygroup.pythonanywhere.com'],
+    domains: [
+      'img.icons8.com', 
+      '127.0.0.1', 
+      'abaygroup.pythonanywhere.com',
+      'abaystreet-filestorage.s3.amazonaws.com'
+    ],
   },
 }
